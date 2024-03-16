75e5f653f66d39c30b342d8b21182a35ea9bab06
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-16 02:49:44 +0100

    Add ux-turbo

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
