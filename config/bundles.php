eb419fdb58c5b3bfa8339a8ce2495a9a7b8f5906
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-16 02:36:36 +0100

    Add ux-autocomplete

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
