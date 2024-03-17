cad3dba426ed28c455fca02a6d8b7a93d32fedce
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-17 13:27:12 +0100

    feat(visit): Add a form type

---
+++ src/Form/VisitType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Patient;
+use App\Entity\Visit;
+use App\Entity\VisitCategory;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class VisitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('patient', EntityType::class, [
+                'class' => Patient::class,
+                'choice_label' =>
+                    fn(Patient $patient) => "{$patient->getFirstName()} {$patient->getFirstName()} {$patient->getLastName()}",
+                'placeholder' => 'Select a patient',
+                'constraints' => [
+                    new NotBlank(['message' => "Please select a patient"]),
+                ],
+            ])
+            ->add('type', EnumType::class, [
+                'class' => VisitCategory::class,
+                'choice_label' => 'displayName',
+                'constraints' => [new NotBlank(['message' => 'Please select a visit type.']),]
+            ])
+            ->add(
+                'diagnosis',
+                options: [
+                    'constraints' => [
+                        new NotBlank(['message' => "Diagnosis cannot be blank"]),
+                        new Regex([
+                            'pattern' => '/^[A-Z]/',
+                            'message' => "Diagnosis must start with a capital letter",
+                        ]),
+                        new Regex([
+                            'pattern' => '/\.$/',
+                            'message' => "Diagnosis must end with a period",
+                        ]),
+                    ],
+                    'help' => "Diagnosis must start with a capital letter and end with a period",
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Visit::class,
+        ]);
+    }
+}
