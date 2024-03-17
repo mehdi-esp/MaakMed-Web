a092f7ae782d4c3296ff0df6f1b9ea3f663c06b6
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-17 20:49:51 +0100

    feat(visit): Implement preliminary visit management with validation and security

---
+++ src/Twig/Components/VisitForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Visit;
+use App\Form\VisitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\Component\Form\FormInterface;
+
+#[AsLiveComponent()]
+final class VisitForm extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    /**
+     * The initial data used to create the form.
+     */
+    #[LiveProp]
+    public ?Visit $initialFormData = null;
+
+    public ?string $buttonLabel = null;
+
+    protected function instantiateForm(): FormInterface
+    {
+        // we can extend AbstractController to get the normal shortcuts
+        return $this->createForm(VisitType::class, $this->initialFormData);
+    }
+}
