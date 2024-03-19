502c1fc675d562654fa0c2988895e863a11e56a2
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-19 13:42:46 +0100

    build: Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DD3B0',
+        secondary: '#086375',
+        accent: '#AFFC41',
+        light: '#B2FF9E',
+        background: '#EDEDED',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+/* 
+ * Our colors
+ *
+ * #1DD3B0
+ * #086375
+ * #AFFC41
+ * #B2FF9E
+ * #EDEDED
+ *
+ * */
