55325a2a927aa3f8cbd96c3b767d92291d4891ce
Mohamed Amor <99400312+mohamedamor002@users.noreply.github.com>
2024-03-28 23:10:10 +0100

    feat(medication):  view Medications

--- src/Controller/MedicationController.php
+++ src/Controller/MedicationController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Medication;
+use App\Form\MedicationType;
+use App\Repository\MedicationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Security\Voter\MedicationVoter;
 
+
+#[Route('/medication')]
 class MedicationController extends AbstractController
 {
-    #[Route('/medication', name: 'app_medication')]
-    public function index(): Response
-    {
+#[Route('/', name: 'app_medication_index', methods: ['GET'])]
+    public function index(MedicationRepository $medicationRepository): Response
+    {    /** @var Doctor|Admin|Pharmacy $user */
+        $user = $this->getUser();
+        $medications= $medicationRepository->findAll();
         return $this->render('medication/index.html.twig', [
-            'controller_name' => 'MedicationController',
+            'medications' => $medications,
         ]);
+    }
+
+    #[Route('/new', name: 'app_medication_new', methods: ['GET', 'POST'])]
+    #[IsGranted("ROLE_ADMIN")]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $medication = new Medication();
+        $form = $this->createForm(MedicationType::class, $medication);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($medication);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'New medication created successfully!');
+
+            return $this->redirectToRoute('app_medication_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        $response = new Response(
+            status: $form->isSubmitted() ?
+                Response::HTTP_UNPROCESSABLE_ENTITY :
+                Response::HTTP_OK,
+        );
+
+        return $this->render('medication/new.html.twig', [
+            'medication' => $medication,
+            'form' => $form->createView(),
+        ], $response);
     }
 }
