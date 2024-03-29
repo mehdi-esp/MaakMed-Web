39d433e00ed4382e4f631f2862a4ebfc65ccb9c4
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-29 13:00:32 +0100

    refactor(invoice): More descriptive criteria name

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -27,12 +27,12 @@ class InvoiceController extends AbstractController
         $user = $this->getUser();
 
         $criteria = [];
-        $userCriteria = match (true) {
+        $visibilityCriterion = match (true) {
             $user instanceof Admin => [],
             $user instanceof Pharmacy => ['pharmacy' => $user],
             $user instanceof Patient => ['patient' => $user],
         };
-        $criteria = array_merge($criteria, $userCriteria);
+        $criteria = array_merge($criteria, $visibilityCriterion);
 
         $invoices = $invoiceRepository->findBy($criteria);
         return $this->render('invoice/index.html.twig', [
