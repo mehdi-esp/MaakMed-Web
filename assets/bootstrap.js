4dfbd86057da905fc06faa1f4ac1c07e8c5bf04d
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-31 13:04:39 +0100

    build: Add stimulus-textarea-autogrow

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 import ScrollTo from '@stimulus-components/scroll-to'
+import TextareaAutogrow from 'stimulus-textarea-autogrow'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -8,5 +9,6 @@ export const app = startStimulusApp(require.context(
     /\.[jt]sx?$/
 ));
 app.register('scroll-to', ScrollTo)
+app.register('textarea-autogrow', TextareaAutogrow)
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
