865f03e8720fd980c7e3ba8516868fa133896a1d
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-03-31 15:12:25 +0100

    build: Add @stimulus-components/scroll-progress

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 import ScrollTo from '@stimulus-components/scroll-to'
 import TextareaAutogrow from 'stimulus-textarea-autogrow'
+import ScrollProgress from '@stimulus-components/scroll-progress'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -10,5 +11,6 @@ export const app = startStimulusApp(require.context(
 ));
 app.register('scroll-to', ScrollTo)
 app.register('textarea-autogrow', TextareaAutogrow)
+app.register('scroll-progress', ScrollProgress)
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
