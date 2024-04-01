fdec14efbf3df36f9e569038e0f7eee0e651a7ca
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-04-01 20:25:27 +0100

    feat(stimulus): Define space key alias

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,6 +2,12 @@ import { startStimulusApp } from '@symfony/stimulus-bridge';
 import ScrollTo from '@stimulus-components/scroll-to'
 import TextareaAutogrow from 'stimulus-textarea-autogrow'
 import ScrollProgress from '@stimulus-components/scroll-progress'
+import { defaultSchema } from "@hotwired/stimulus"
+
+const customSchema = {
+  ...defaultSchema,
+  keyMappings: { ...defaultSchema.keyMappings, space: " " },
+}
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -9,6 +15,7 @@ export const app = startStimulusApp(require.context(
     true,
     /\.[jt]sx?$/
 ));
+app.schema = customSchema;
 app.register('scroll-to', ScrollTo)
 app.register('textarea-autogrow', TextareaAutogrow)
 app.register('scroll-progress', ScrollProgress)
