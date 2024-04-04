ae5c560501ee4ad69f58ee6f02f0a98817176d48
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-04-04 16:14:49 +0100

    build: Add @stimulus-components/popover

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,6 +2,7 @@ import { startStimulusApp } from '@symfony/stimulus-bridge';
 import ScrollTo from '@stimulus-components/scroll-to'
 import TextareaAutogrow from 'stimulus-textarea-autogrow'
 import ScrollProgress from '@stimulus-components/scroll-progress'
+import Popover from '@stimulus-components/popover'
 import { defaultSchema } from "@hotwired/stimulus"
 
 const customSchema = {
@@ -20,5 +21,6 @@ app.schema = customSchema;
 app.register('scroll-to', ScrollTo)
 app.register('textarea-autogrow', TextareaAutogrow)
 app.register('scroll-progress', ScrollProgress)
+app.register('popover', Popover)
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
