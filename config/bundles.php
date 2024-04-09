4ed604789968c1dc8618185220244aae7264e516
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-04-09 20:35:27 +0100

    fixup! build: Add knp-time-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,5 @@ return [
     PixelOpen\CloudflareTurnstileBundle\PixelOpenCloudflareTurnstileBundle::class => ['all' => true],
     Jungi\FrameworkExtraBundle\JungiFrameworkExtraBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
