a15f6906ceb45a963dda8470761f34ceb7ea9849
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-04-12 00:43:33 +0100

    fix(speech): Reduce debounce time

--- assets/controllers/speech_controller.js
+++ assets/controllers/speech_controller.js
@@ -18,7 +18,7 @@ export default class extends Controller {
     static debounces = [
         {
             name: "playInstructionSound",
-            wait: 3000,
+            wait: 500,
         }
     ];
     static throttles = [
