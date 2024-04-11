fbab417bfb9be22993176cc6261a1d418274532c
mohamedamor002 <mohamed.amor@esprit.tn>
2024-04-11 23:40:43 +0100

    fix form validation for medication and Prescription
    from server side

--- src/Form/MedicationType.php
+++ src/Form/MedicationType.php
@@ -6,14 +6,41 @@ use App\Entity\Medication;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MedicationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', null, [
+                'constraints' => [
+                    new Assert\Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'The name must be at least {{ limit }} characters long',
+                        'maxMessage' => 'The name cannot be longer than {{ limit }} characters',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][a-zA-Z0-9]*$/',
+                        'message' => 'The name should start with a capital letter and should not contain special characters',
+                    ]),
+                ],
+            ])
+            ->add('description', null, [
+                'constraints' => [
+                    new Assert\Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'The description must be at least {{ limit }} characters long',
+                        'maxMessage' => 'The description cannot be longer than {{ limit }} characters',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Z][a-zA-Z0-9]*$/',
+                        'message' => 'The description should start with a capital letter and should not contain special characters',
+                    ]),
+                ],
+            ])
             ->add('insured')
         ;
     }
