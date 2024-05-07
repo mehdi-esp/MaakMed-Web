469e0eb9ef0c24ee06c671a2e3573dc0e25e971f
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-05-07 09:41:13 +0100

    feat(fixtures): Add more fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -41,5 +41,72 @@ class AppFixtures extends Fixture
                 'username' => 'admino'
             ]);
         });
+
+        // One invoice
+
+        $meds = ['Panadol', 'Phenylephrine', 'Ibuprofen', 'Coreg', 'Amoxicillin', 'Ativan100mg', 'Adderalll'];
+
+        $invoice = InvoiceFactory::createOne([
+            'pharmacy' => PharmacyFactory::find(['username' => 'fortis']),
+            'invoiceEntries' => InvoiceEntryFactory::randomRange(0, 0),
+        ]);
+
+        InvoiceEntryFactory::createOne([
+            'invoice' => $invoice,
+            'medication' => MedicationFactory::findOrCreate(['name' => 'Panadol']),
+        ]);
+
+        InvoiceEntryFactory::createOne([
+            'invoice' => $invoice,
+            'medication' => MedicationFactory::findOrCreate(['name' => 'Phenylephrine']),
+        ]);
+
+        InvoiceEntryFactory::createOne([
+            'invoice' => $invoice,
+            'medication' => MedicationFactory::findOrCreate(['name' => 'Ibuprofen']),
+        ]);
+
+        Factory::delayFlush(function () {
+            VisitFactory::createMany(5, [
+                'doctor' => DoctorFactory::find(['username' => 'gamil']),
+                'patient' => PatientFactory::find(['username' => 'anwar']),
+            ]);
+            VisitFactory::createMany(5, [
+                'doctor' => DoctorFactory::find(['username' => 'gamil']),
+            ]);
+            VisitFactory::createMany(5, [
+                'patient' => PatientFactory::find(['username' => 'anwar']),
+            ]);
+        });
+
+        // Prescriptions
+
+        $prescription = PrescriptionFactory::createOne([
+            'medications' => PrescriptionEntryFactory::randomRange(0, 0),
+        ]);
+
+        foreach ($meds as $medName) {
+            PrescriptionEntryFactory::createOne([
+                'prescription' => $prescription,
+                'medication' => MedicationFactory::findOrCreate(['name' => $medName]),
+            ]);
+        }
+
+        // Subscriptions
+        SubscriptionFactory::createMany(20, fn () => [
+            'patient' => PatientFactory::createOne(),
+            'status' => 'active'
+        ]);
+
+        SubscriptionFactory::createMany(10, ['status' => 'canceled']);
+
+        // Inventory
+
+        foreach ($meds as $medName) {
+            InventoryEntryFactory::createOne([
+                'medication' => MedicationFactory::findOrCreate(['name' => $medName]),
+                'pharmacy' => PharmacyFactory::find(['username' => 'fortis']),
+            ]);
+        }
     }
 }
