fb1ef1c0c1359da71c21b6841532c44ee3365fd6
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-05-07 07:35:34 +0100

    feat(fixtures): Define a well known user for each type

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AdminFactory;
+use App\Factory\DoctorFactory;
+use App\Factory\InsurancePlanFactory;
+use App\Factory\PatientFactory;
+use App\Factory\PharmacyFactory;
+use App\Factory\VisitFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Zenstruck\Foundry\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        Factory::delayFlush(function () {
 
-        $manager->flush();
+            // Known users
+
+            PatientFactory::createOne([
+                'firstName' => 'Anwar',
+                'lastName' => 'Hussain',
+                'username' => 'anwar',
+            ]);
+
+            DoctorFactory::createOne([
+                'firstName' => 'Gamil',
+                'lastName' => 'Arfan',
+                'username' => 'gamil',
+            ]);
+
+            PharmacyFactory::createOne([
+                'name' => 'Fortis Pharmacy',
+                'username' => 'fortis'
+            ]);
+
+            AdminFactory::createOne([
+                'username' => 'admino'
+            ]);
+        });
     }
 }
