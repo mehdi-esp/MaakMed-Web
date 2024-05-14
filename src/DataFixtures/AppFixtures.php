2a711390d9b7fc6527037f0ce3a698f7ef4886d0
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2024-05-14 11:41:21 +0100

    feat: Add more fixture insertions

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -40,6 +40,13 @@ class AppFixtures extends Fixture
                 'username' => 'gamil',
             ]);
 
+
+            DoctorFactory::createOne([
+                'firstName' => 'Nawal',
+                'lastName' => 'Bileli',
+                'username' => 'nawal',
+            ]);
+
             PharmacyFactory::createOne([
                 'name' => 'Fortis Pharmacy',
                 'username' => 'fortis'
@@ -48,42 +55,149 @@ class AppFixtures extends Fixture
             AdminFactory::createOne([
                 'username' => 'admino'
             ]);
+
+            // Plans
+
+            InsurancePlanFactory::createOne([
+                'name' => 'Silver',
+                'description' => 'Entry tier',
+                'cost' => 200.23,
+                'reimbursementRate' => 0.15,
+                'ceiling' => 113.71,
+            ]);
+
+            InsurancePlanFactory::createOne([
+                'name' => 'Gold',
+                'description' => 'Mid tier',
+                'cost' => 599.95,
+                'reimbursementRate' => 0.67,
+                'ceiling' => 354.53,
+            ]);
+
+            InsurancePlanFactory::createOne([
+                'name' => 'Platinum',
+                'description' => 'Most profitable',
+                'cost' => 1799.99,
+                'reimbursementRate' => 0.56,
+                'ceiling' => 1199.95,
+            ]);
+
+            // Medications
+
+            MedicationFactory::createOne(
+                [
+                    'name' =>  'Acetaminophen',
+                    'description' =>  'Acetaminophen also known as paracetamol is a common over the counter medication used to relieve pain and reduce fever. It works by inhibiting the production of prostaglandins in the brain  which are chemicals involved in pain perception and the regulation of body temperature. Acetaminophen is often used to alleviate headaches muscle aches arthritis pain  and discomfort associated with colds and fevers.',
+                ]
+            );
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Adderalll',
+                    'description' =>   'Adderall is a prescription medication used to treat attention deficit hyperactivity disorder  and narcolepsy. It contains a combination of amphetamine and dextroamphetamine which work by increasing the levels of certain neurotransmitters in the brain improving focus attention and impulse control. Common side effects include insomnia loss of appetite and increased heart rate. It can be habit forming and should be used cautiousl',
+                ]
+            );
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Ativan100mg',
+                    'description' => 'Ativan is a benzodiazepine medication used to treat anxiety disorders and short term relief of symptoms. It works by enhancing the effects of a neurotransmitter in the brain producing a calming effect. Side effects may include drowsiness dizziness and confusion. It s typically prescribed for short term use due to the risk of dependence and withdrawal symptoms',
+                ]
+            );
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Amoxicillin',
+                    'description' => 'Amoxicillin is an antibiotic used to treat bacterial infections like ear infections pneumonia  and strep throat. It works by stopping bacteria from multiplying. Side effects may include nausea diarrhea and rash. Finish the full course as directed by your doctor to prevent antibiotic resistance',
+                ]
+            );
+            MedicationFactory::createOne(
+                [
+                    'name' =>  'Coreg',
+                    'description' => 'Coreg is a medication for heart failure and high blood pressure. It belongs to a class of drugs called beta blockers which work by relaxing blood vessels and slowing heart rate to improve blood flow and reduce blood pressure. Side effects may include dizziness fatigue and low blood pressure. Take as prescribed by your doctor and monitor for any signs of worsening heart failure or side effects',
+                ]
+            );
+
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Panadol',
+                    'description' => 'Panadol is a brand name for acetaminophen a common over the counter medication used to relieve pain and reduce fever. It works by inhibiting the production of prostaglandins in the brain  which are chemicals involved in pain perception and the regulation of body temperature. Panadol is often used to alleviate headaches muscle aches arthritis pain  and discomfort associated with colds and fevers.',
+                ]
+            );
+
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Ibuprofen',
+                    'description' => 'Ibuprofen is a nonsteroidal anti inflammatory drug NSAID used to treat pain and reduce fever. It works by blocking the production of prostaglandins in the body which are chemicals involved in pain inflammation and fever. Common side effects include stomach upset and ulcers. Take with food or milk to reduce stomach irritation',
+
+                ]
+            );
+
+            MedicationFactory::createOne(
+                [
+                    'name' => 'Phenylephrine',
+                    'description' => 'Phenylephrine is a decongestant used to relieve nasal congestion due to colds allergies and sinus infections. It works by constricting blood vessels in the nasal passages to reduce swelling and congestion. Side effects may include headache dizziness and nervousness. It should be used cautiously in patients with high blood pressure and heart disease.'
+                ]
+            );
         });
 
-        // One invoice
+        // Used medications
 
-        $meds = ['Panadol', 'Phenylephrine', 'Ibuprofen', 'Coreg', 'Amoxicillin', 'Ativan100mg', 'Adderalll'];
+        $meds = [
+            'Acetaminophen',
+            'Adderalll',
+            'Ativan100mg',
+            'Amoxicillin',
+            'Coreg',
+            'Panadol',
+            'Ibuprofen',
+            'Phenylephrine',
+        ];
+
+        // One invoice for the fortis pharmacy
 
         $invoice = InvoiceFactory::createOne([
             'pharmacy' => PharmacyFactory::find(['username' => 'fortis']),
             'invoiceEntries' => InvoiceEntryFactory::randomRange(0, 0),
         ]);
 
-        InvoiceEntryFactory::createOne([
-            'invoice' => $invoice,
-            'medication' => MedicationFactory::findOrCreate(['name' => 'Panadol']),
+        foreach (array_rand(array_flip($meds), rand(intdiv(count($meds), 2), count($meds))) as $medName) {
+            InvoiceEntryFactory::createOne([
+                'invoice' => $invoice,
+                'medication' => MedicationFactory::findOrCreate(['name' => $medName]),
+            ]);
+        }
+
+
+        // Subscriptions and patients
+
+        SubscriptionFactory::createMany(20, fn () => [
+            'patient' => PatientFactory::createOne(),
+            'status' => 'active'
         ]);
 
-        InvoiceEntryFactory::createOne([
-            'invoice' => $invoice,
-            'medication' => MedicationFactory::findOrCreate(['name' => 'Phenylephrine']),
-        ]);
+        SubscriptionFactory::createMany(10, ['status' => 'canceled']);
 
-        InvoiceEntryFactory::createOne([
-            'invoice' => $invoice,
-            'medication' => MedicationFactory::findOrCreate(['name' => 'Ibuprofen']),
-        ]);
+
+        // Visits
 
         Factory::delayFlush(function () {
-            VisitFactory::createMany(5, [
+
+            VisitFactory::createOne([
                 'doctor' => DoctorFactory::find(['username' => 'gamil']),
                 'patient' => PatientFactory::find(['username' => 'anwar']),
+                'diagnosis' => 'Patient has fever, their temperature is 38.5C.',
             ]);
-            VisitFactory::createMany(5, [
-                'doctor' => DoctorFactory::find(['username' => 'gamil']),
-            ]);
+
             VisitFactory::createMany(5, [
                 'patient' => PatientFactory::find(['username' => 'anwar']),
+            ]);
+
+            VisitFactory::createMany(5, [
+                'doctor' => DoctorFactory::find(['username' => 'gamil']),
+                'patient' => PatientFactory::new()
+            ]);
+
+            VisitFactory::createMany(5, [
+                'doctor' => DoctorFactory::find(['username' => 'nawal']),
+                'patient' => PatientFactory::new()
             ]);
         });
 
@@ -93,20 +207,12 @@ class AppFixtures extends Fixture
             'medications' => PrescriptionEntryFactory::randomRange(0, 0),
         ]);
 
-        foreach ($meds as $medName) {
+        foreach (array_rand(array_flip($meds), rand(2, count($meds))) as $medName) {
             PrescriptionEntryFactory::createOne([
                 'prescription' => $prescription,
                 'medication' => MedicationFactory::findOrCreate(['name' => $medName]),
             ]);
         }
-
-        // Subscriptions
-        SubscriptionFactory::createMany(20, fn () => [
-            'patient' => PatientFactory::createOne(),
-            'status' => 'active'
-        ]);
-
-        SubscriptionFactory::createMany(10, ['status' => 'canceled']);
 
         // Inventory
 
@@ -116,5 +222,78 @@ class AppFixtures extends Fixture
                 'pharmacy' => PharmacyFactory::find(['username' => 'fortis']),
             ]);
         }
+
+        // Issues
+
+        Factory::delayFlush(function () {
+            IssueFactory::createOne(
+                [
+                    'title' => 'problem',
+                    'content' => "I can't add an issue",
+                    'creationDate' => new \DateTimeImmutable('-1 days'),
+                    'category' => 'Other'
+                ]
+            );
+
+            IssueFactory::createOne(
+                [
+                    'title' => 'payment',
+                    'content' => "payment page is still loading",
+                    'creationDate' => new \DateTimeImmutable('-3 days'),
+                    'category' => 'Other'
+                ]
+            );
+
+            IssueFactory::createOne([
+                'title' => 'late',
+                'content' => 'The doctor was late to our appointment',
+                'category' => 'Doctor'
+            ]);
+
+            IssueFactory::createOne([
+                'title' => 'Medication',
+                'content' => 'The pharmacist gave me the wrong amount of medication',
+                'creationDate' => new \DateTimeImmutable('-3 days'),
+                'category' => 'Pharmacy'
+            ]);
+
+            IssueFactory::createOne(
+                [
+                    'title' => 'profile',
+                    'content' => "I cant display my profile page",
+                    'creationDate' => new \DateTimeImmutable('-10 days'),
+                    'category' => 'Medication'
+
+                ]
+            );
+
+            IssueFactory::createOne(
+                [
+                    'title' => 'cancel',
+                    'content' => "I can't cancel my subscription",
+                    'creationDate' => new \DateTimeImmutable('-7 days'),
+                    'category' => 'Medication'
+
+                ]
+            );
+
+            IssueFactory::createOne(
+                [
+                    'title' => 'error',
+                    'content' => "I can't display the user page",
+                    'creationDate' => new \DateTimeImmutable('-4 days'),
+                    'category' => 'Other'
+                ]
+            );
+
+            IssueFactory::createOne(
+                [
+                    'title' => 'inventory',
+                    'content' => "I can't add a medication to my inventory",
+                    'creationDate' => new \DateTimeImmutable('-2 days'),
+                    'category' => 'Pharmacy'
+                ]
+            );
+        });
     }
 }
